805fb4a54013de5eb2fdf5c774139bcd5efdf014
LuanCalais <luancalais@gmail.com>
2023-04-09 22:15:23 -0300

    prepare initial scream from heroes

--- src/Pages/landPage/index.js
+++ src/Pages/landPage/index.js
@@ -3,9 +3,9 @@ import "./LandPage.css";
 const LandPage = () => {
   return (
     <>
-        <div>Oi</div>
-        <div>Tchau</div>
-    
+      <div className="container">
+        <section></section>
+      </div>
     </>
   );
 };
