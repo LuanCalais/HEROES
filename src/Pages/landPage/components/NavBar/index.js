53d55e6526e9279a0adb1b9f7b82bc5a3cb8916f
LuanCalais <luancalais@gmail.com>
2023-04-10 22:24:01 -0300

    fix button position

--- src/Pages/landPage/components/NavBar/index.js
+++ src/Pages/landPage/components/NavBar/index.js
@@ -3,7 +3,7 @@ import "./NavBar.css";
 const NavBar = () => {
   return (
     <div className="nav-menu">
-      <button>Entrar</button>
+      <button className="btn">Entrar</button>
     </div>
   );
 };
